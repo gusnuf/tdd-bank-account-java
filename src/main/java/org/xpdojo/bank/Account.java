0b1fe0671492e7416d5d49af6985ef90ec119fe9
Grahame Murray <grahame@grahame.com>
2021-01-28 10:10:56 -0500

    Parity with Ben and Toby's session

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,14 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    protected int current_balance = 0;
+
+    public void deposit(int amount) {
+        current_balance += amount;
+    }
+
+    public int balance() {
+        return current_balance;
+    }
 }
